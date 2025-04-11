be84e781eb36539de1962ede43dea522ab7c9181
Zakeer Hussain <zak.ican4d@gmail.com>
2025-04-11 23:43:15 +0530

    shell script learning

--- 01-variables.sh
+++ 01-variables.sh
@@ -4,4 +4,4 @@ echo "Please enter username"
 
 read -s Username
 
-echo "Username is : ${Username}" # printing here just for validation, we should not print on the terminal.
+echo "Username is : ${Username}" # printing here just for validation, we should not print on the terminal....
