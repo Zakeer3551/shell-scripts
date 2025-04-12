697cbfb65c4a1437895abb9d6ca3d79a3ed886fb
Zakeer Hussain <zak.ican4d@gmail.com>
2025-04-12 14:32:51 +0530

    shell-script for variables

---
+++ 02-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS = ("Apple" "Banana" "Mango")
+
+echo "First value is ${FRUITS[0]}"
+
+echo "Second value is ${FRUITS[1]}"
+
+echo "Third value is ${FRUITS[2]}"
+
+echo "All Fruits: ${FRUITS[@]}"
