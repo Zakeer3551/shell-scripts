c8a5c93cb2a3e598b3ba8d12f95c325ffdc5fc7a
Zakeer Hussain <zak.ican4d@gmail.com>
2025-04-12 15:32:19 +0530

    shell-script for variables

--- 05-functions.sh
+++ 05-functions.sh
@@ -3,12 +3,12 @@
 ID=$(id -u)
 
 VALIDATE(){
-    if [ $? -ne 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo " ERROR: Installing is failed "
+        echo " ERROR: $2 is failed "
         exit 1
     else
-        echo " Installation is successful"
+        echo " $2 is successful"
 fi
 }
 
@@ -22,12 +22,12 @@ fi
 
 yum install mysql -y
 
-VALIDATE
+VALIDATE $? "Installing MYSQL"
 
 yum install git -y
 
-VALIDATE
+VALIDATE $? "Installing git"
 
 yum install nginx -y
 
-VALIDATE
+VALIDATE $? "Installing nginx"
