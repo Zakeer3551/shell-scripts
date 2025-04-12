2ea543e219656ad7f0f312846778ff2f467b426a
Zakeer Hussain <zak.ican4d@gmail.com>
2025-04-12 16:06:19 +0530

    shell-script for variables

---
+++ 06-logs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo " script name: $0 " 
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo " ERROR: $2 is failed "
+        exit 1
+    else
+        echo " $2 is successful"
+fi
+}
+
+if [ $ID -gt 0 ]
+then
+    echo " ERROR: Please run this with root user "
+    exit 1
+else 
+    echo " You are root user "
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing MYSQL"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
+
+yum install nginx -y
+
+VALIDATE $? "Installing nginx"
