52b5346e283f9de4755b93359d8f73f34e984d5b
Zakeer Hussain <zak.ican4d@gmail.com>
2025-04-13 14:18:19 +0530

    shell-script for variables

--- 06-logs.sh
+++ 06-logs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F:%r)
 
-echo " script name: $0 " 
+LOG_FILE="/tmp/$0-$TIMESTAMP.log"
+
+# echo " script name: $0 " #$0 this special variable gives script name 
 
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+    if [ $1 -ne 0 ] # $1, $2, these are arugments upto $n nth arguement we can give 
     then
         echo " ERROR: $2 is failed "
         exit 1
@@ -23,14 +26,14 @@ else
     echo " You are root user "
 fi
 
-yum install mysql -y
+yum install mysql -y  $>> $LOG_FILE
 
 VALIDATE $? "Installing MYSQL"
 
-yum install git -y
+yum install git -y $>> $LOG_FILE
 
 VALIDATE $? "Installing git"
 
-yum install nginx -y
+yum install nginx -y $>> $LOG_FILE
 
 VALIDATE $? "Installing nginx"
