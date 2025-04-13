7702ac93e0db34ce0aab92a03740ba9fba3deb43
Zakeer Hussain <zak.ican4d@gmail.com>
2025-04-13 14:49:36 +0530

    shell-script for variables

--- 06-logs.sh
+++ 06-logs.sh
@@ -14,19 +14,19 @@ LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ] # $1, $2, these are arugments upto $n nth arguement we can give 
     then
-        echo -e " ERROR: $2 is $R FAILED $N"
+        echo -e "ERROR: $2 is $R FAILED $N"
         exit 1
     else
-        echo " $2 is $R SUCCESS $N"
+        echo "$2 is $R SUCCESS $N"
 fi
 }
 
 if [ $ID -gt 0 ]
 then
-    echo -e " $R ERROR: $N Please run this with root user "
+    echo -e "$R ERROR: $N Please run this with root user"
     exit 1
 else 
-    echo -e " $G You are root user $N"
+    echo -e "$G You are root user $N"
 fi
 
 yum install mysql -y  &>> $LOG_FILE
