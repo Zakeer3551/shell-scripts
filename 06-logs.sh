52f983531cf46fd7d8d873e2a829b1a7e9d8fb4b
Zakeer Hussain <zak.ican4d@gmail.com>
2025-04-13 15:39:16 +0530

    shell-script for variables

--- 06-logs.sh
+++ 06-logs.sh
@@ -12,7 +12,7 @@ LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 
 
 VALIDATE(){
-    if [ $1 -ne 0 ] # $1, $2, these are arugments upto $n nth arguement we can give 
+    if [ $1 -ne 0 ]  
     then
         echo -e "ERROR: $2 is $R FAILED $N"
         exit 1
@@ -31,7 +31,8 @@ fi
 
 yum install mysql -y  &>> $LOG_FILE
 
-VALIDATE $? "Installing MYSQL"
+VALIDATE $? "Installing MYSQL" # $1, $2, these are arugments which we are passing to up(line 15 validate function) so there $1 = $? means ($? is the exit status of previous command (here in this case previous command is yum install mysql -y the exit status is 0 (0 means success)) and $2 = "Installing MYSQL") son in line 17,20 - $2 comes like "Installing MYSQL" 
+# so if we add one more thing after "Installing MYSQL" it will become 3rd arugment ($3 argument) and we call it in validate function as $3) 
 
 yum install git -y &>> $LOG_FILE
 
